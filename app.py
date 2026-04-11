8cb6cbafad41658793724d007a2080fb61c5505b
jojo <jojo@domain.com>
2026-04-11 19:02:46 +0000

    init project

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+reservations = []
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/reserve')
+def reserve():
+    return render_template('reserve.html')
+
+@app.route('/list')
+def list_page():
+    return render_template('list.html', reservations=reservations)
+
+@app.route('/add', methods=['POST'])
+def add():
+    reservations.append({
+        "name": request.form['name'],
+        "car": request.form['car'],
+        "date": request.form['date']
+    })
+    return redirect('/list')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
